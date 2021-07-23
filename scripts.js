3ca3308a120dc669ed446e324e91c2d597941a8f
Ohtico <viticogelvis5@gmail.com>
2021-07-23 14:24:51 -0500

    en proceso

---
+++ scripts.js
@@ -0,0 +1,103 @@
+
+
+ numero = document.getElementById('numeroIntro').value;
+
+
+function convertidor(num){
+ //DECLARION DE VARIABLES
+
+ var romano = '';
+ romano += num;
+ resultado=[];
+ answer1 = '';
+ answer2='';
+ answer3='';
+ answer4='';
+ if (str.length == 0){
+    result = [];
+}else if (str.length > 0){
+ switch (str[str.length-1]){
+        case 1:
+        answer1 = "I";
+          break;
+        case 2:
+        answer1 = "II";
+          break;
+        case 3:
+        answer1 = "III";
+            break;
+        case 4:
+          answer1 = "IV";
+          break;
+        case 5:
+        answer1 = "V";
+          break;
+        case 6:
+        answer1 = "VI";
+          break;
+        case 7:
+        answer1 = "VII";
+          break;
+        case 8:
+        answer1 = "VIII";
+            break;
+        case 8:
+        answer1 = "IX";
+            break;
+        case 8:
+        answer1 = "X";
+            break;
+}
+switch (str[str.length-2]){
+    
+
+
+
+
+function cuadroText() {
+    numero = document.getElementById('numeroIntro').value;
+    respues = convertidor(numero);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function obtenerTexto(){
+    return document.getElementById('cuadroText').value
+  }
+
+  function convertirRomanos(){
+    const texto = obtenerTexto()
+    const romano = esPalindromo(val)
+   
+    mostrarMensaje(romano)
+  }
+
+  function esRomano(val){
+    switch(val) {
+        
+      }
+      return val = answer
+    }
+    function mostrarMensaje(value, romano){
+        swal.fire({
+          title: value ? `La palabra . Si es un palindromo ` : `La palabra . No es un palindromo `,
+          icon: `info`,
+          confirmButtonText: `Entendido`,
+          
+          
+        })
+      }*/
